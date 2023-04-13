98072a8bc3dbfeddc69035b52f98ea7ac3f2d7b7
N-eeraj <neeraj7rajeena@gmail.com>
2023-04-13 17:26:21 +0530

    Template & basic portrait styles for error page

--- error.vue
+++ error.vue
@@ -8,6 +8,14 @@ useMeta('Page not found')
             src="/images/404.png"
             alt="Sleeping Snorlax"
             class="error-image" />
+        <div class="text-container">
+            <h3 class="message">
+                A sleeping Snorlax blocked your path !
+            </h3>
+            <button class="redirect-btn">
+                Go Back
+            </button>
+        </div>
     </div>
 </template>
 
