8ad2c72b1e449da07b5aa34422b60419f943892b
N-eeraj <neeraj7rajeena@gmail.com>
2023-04-16 02:16:24 +0530

    Added function to add/remove at-top class based on scroll position

--- components/Nav/Bar.vue
+++ components/Nav/Bar.vue
@@ -1,9 +1,30 @@
 <script setup>
 import navLinks from '@/data/navLinks'
+
+const navBar = ref(null)
+
+const checkAtTop = () => {
+    const { clientHeight: navBarHeight, classList: navBarClassList } = navBar.value
+    if (window.pageYOffset > navBarHeight)
+        navBarClassList.remove('at-top')
+    else
+        navBarClassList.add('at-top')
+}
+
+onMounted(() => {
+    checkAtTop()
+    window.addEventListener('scroll', checkAtTop)
+})
+
+onBeforeUnmount(() => {
+    window.removeEventListener('scroll', () => {})
+})
 </script>
 
 <template>
-    <nav class="nav-bar">
+    <nav
+        ref="navBar"
+        class="nav-bar at-top">
         <ul class="nav-list">
             <NavItem
                 v-for="({ to, text, icon, className }, index) in navLinks"
