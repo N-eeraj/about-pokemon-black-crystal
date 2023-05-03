3ab2cc2f4572b447c8107a3aca44b80adc025acf
N-eeraj <neeraj7rajeena@gmail.com>
2023-05-03 21:54:24 +0530

    Portrait view for arcade mode events

---
+++ components/Home/ArcadeMode/Card.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+    icon: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="card">
+        <img
+            :src="`icons/events/${icon}.svg`"
+            :alt="icon"
+            class="icon">
+        <article class="text-container">
+            <strong class="sub-title">
+                {{ title }}
+            </strong>
+            <p class="description">
+                {{ description }}
+            </p>
+        </article>
+    </div>
+</template>
+
+<style
+    src="@styles/components/home/arcade-mode/card.scss"
+    lang="scss"
+    scoped>
+</style>
--- components/Home/ArcadeMode/index.vue
+++ components/Home/ArcadeMode/index.vue
@@ -1,3 +1,7 @@
+<script setup>
+import arcadeEvents from '@data/arcadeEvents'
+</script>
+
 <template>
     <section id="arcade-mode">
         <div class="text-container">
@@ -7,6 +11,16 @@
             <p class="content">
                 Experience Pokémon trainer battles outside the Story Mode.
             </p>
+        </div>
+
+        <div class="event-container">
+            <HomeArcadeModeCard
+                v-for="({icon, title, description}, index) in arcadeEvents"
+                :icon="icon"
+                :title="title"
+                :description="description"
+                :class="icon"
+                :key="index" />
         </div>
     </section>
 </template>
