90bc33b382d463a39f3849a99ada53d1cf129905
N-eeraj <neeraj7rajeena@gmail.com>
2023-05-08 14:18:58 +0530

    Layout style updations

--- components/Help/Tab/index.vue
+++ components/Help/Tab/index.vue
@@ -1,9 +1,12 @@
 <script setup>
 import tabs from '@data/help/tabs'
 
+const emit =  defineEmits(['change'])
 const activetab = ref(null)
 
 const handleSelect = value => activetab.value = value
+
+watch(activetab, () => emit('change', activetab.value))
 
 onMounted(() => {
     activetab.value = tabs[0].value
@@ -11,13 +14,15 @@ onMounted(() => {
 </script>
 
 <template>
-    <HelpTabItem
-        v-for="({ text, value }, index) in tabs"
-        :text="text"
-        :value="value"
-        :is-active="activetab === value"
-        :key="index"
-        @select="handleSelect" />
+    <div class="tab-container">
+        <HelpTabItem
+            v-for="({ text, value }, index) in tabs"
+            :text="text"
+            :value="value"
+            :is-active="activetab === value"
+            :key="index"
+            @select="handleSelect" />
+    </div>
 </template>
 
 <style
--- pages/help.vue
+++ pages/help.vue
@@ -1,7 +1,19 @@
 <script setup>
 useMeta('Help')
+
+const handleTabChange = tab => {
+  console.log(tab)
+}
 </script>
 
 <template>
-  <HelpTab />
+  <div class="tab-container">
+    <HelpTab @change="handleTabChange" />
+  </div>
 </template>
+
+<style
+    src="@styles/pages/help.scss"
+    lang="scss"
+    scoped>
+</style>
