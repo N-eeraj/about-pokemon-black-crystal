bc00b8dabedd4306cb7dbdc749e8a33f4b26c286
N-eeraj <neeraj7rajeena@gmail.com>
2023-05-13 03:57:28 +0530

    Added gyms faq

--- data/help/faq/index.js
+++ data/help/faq/index.js
@@ -1,13 +1,18 @@
 import pokemon from '@data/help/faq/answers/pokemon.html?raw'
 import pokedex from '@data/help/faq/answers/pokedex.html?raw'
+import gyms from '@data/help/faq/answers/gyms.html?raw'
 
 export default [
     {
-        question: 'What is a Pokémon ?',
+        question: 'What is a Pokémon?',
         answer: pokemon
     },
     {
-        question: 'What is a Pokédex ?',
+        question: 'What is a Pokédex?',
         answer: pokedex
+    },
+    {
+        question: 'What are Gyms & Gym Badges?',
+        answer: gyms
     }
 ]
