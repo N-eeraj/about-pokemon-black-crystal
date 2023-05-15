18920b6af284660afb48e011f1eb57ef120788d6
N-eeraj <neeraj7rajeena@gmail.com>
2023-05-16 01:03:37 +0530

    Added more FAQ and persist FAQ open via query params

--- components/Help/Faq/index.vue
+++ components/Help/Faq/index.vue
@@ -1,14 +1,25 @@
 <script setup>
 import faqs from '@data/help/faq'
 
+const route = useRoute()
+const router = useRouter()
 const currentFAQ = ref(null)
 
 const handleChange = (index) => {
-    if (currentFAQ.value === index)
+    if (currentFAQ.value === index) {
         currentFAQ.value = null
-    else
+        router.replace({ query: { faq: index } })
+    }
+    else {
         currentFAQ.value = index
+        router.replace({ query: { ...route.query, qNo: index + 1 } })
+    }
 }
+
+onMounted(() => {
+    if (route.query.qNo)
+        handleChange(route.query.qNo - 1)
+})
 </script>
 
 <template>
--- data/help/faq/index.js
+++ data/help/faq/index.js
@@ -1,6 +1,11 @@
 import pokemon from '@data/help/faq/answers/pokemon.html?raw'
 import pokedex from '@data/help/faq/answers/pokedex.html?raw'
 import gyms from '@data/help/faq/answers/gyms.html?raw'
+import party from '@data/help/faq/answers/party.html?raw'
+import pc from '@data/help/faq/answers/pc.html?raw'
+import release from '@data/help/faq/answers/release.html?raw'
+import levelUp from '@data/help/faq/answers/level-up.html?raw'
+import evolution from '@data/help/faq/answers/evolution.html?raw'
 
 export default [
     {
@@ -14,5 +19,25 @@ export default [
     {
         question: 'What are Gyms & Gym Badges?',
         answer: gyms
+    },
+    {
+        question: 'What is \"Party Pokémon\"?',
+        answer: party
+    },
+    {
+        question: 'What is the PC?',
+        answer: pc
+    },
+    {
+        question: 'What does it mean to release a Pokémon?',
+        answer: release
+    },
+    {
+        question: 'How to make my Pokémon stronger?',
+        answer: levelUp
+    },
+    {
+        question: 'How to evolve my Pokémon?',
+        answer: evolution
     }
 ]
