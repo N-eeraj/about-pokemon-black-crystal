f9c9545900b40560b23713082beb39a17f5e95b3
N-eeraj <neeraj7rajeena@gmail.com>
2023-05-24 13:47:20 +0530

    Basic setup for videos section

---
+++ components/Help/Videos/Item.vue
@@ -0,0 +1,9 @@
+<template>
+    
+</template>
+
+<style
+    src="@styles/components/help/videos/item.scss"
+    lang="scss"
+    scoped>
+</style>
--- components/Help/Videos/index.vue
+++ components/Help/Videos/index.vue
@@ -1,5 +1,5 @@
 <template>
-    video
+    
 </template>
 
 <style
