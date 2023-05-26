7e279897f1fa4a28a4c2b5ff02117da66d1e0697
N-eeraj <neeraj7rajeena@gmail.com>
2023-05-26 13:25:27 +0530

    Added pause & play functionalities with sample video

--- components/Help/Videos/Item.vue
+++ components/Help/Videos/Item.vue
@@ -1,15 +1,53 @@
 <script setup>
-defineProps({
+const props = defineProps({
+    url: {
+        type: String,
+        required: true
+    },
     title: {
         type: String,
         required: true
+    },
+    play: {
+        type: Boolean,
+        required: false,
+        default: false
     }
 })
+
+const emit = defineEmits([
+    'pause',
+    'play'
+])
+
+const video = ref(null)
+
+watch(() => props.play, newValue => {
+    if (!newValue)
+        video.value.pause()
+})
+
+const handleVideoClick = () => {
+    if (props.play) {
+        emit('pause')
+        video.value.pause()
+    }
+    else {
+        emit('play')
+        video.value.play()
+    }
+}
 </script>
 
 <template>
-    <div class="video-item">
-        <video class="video">
+    <div
+        class="video-item"
+        @click="handleVideoClick">
+        <video
+            ref="video"
+            :src="url"
+            :autoplay="play"
+            class="video" >
         </video>
         <div class="overlay">
             <span class="title">
--- components/Help/Videos/index.vue
+++ components/Help/Videos/index.vue
@@ -1,12 +1,26 @@
 <script setup>
 import videos from '@data/help/videos'
+
+const currentPlaying = ref(null)
+
+const handlePause = () => {
+    currentPlaying.value = null
+}
+
+const handlePlay = index => {
+    currentPlaying.value = index
+}
 </script>
 
 <template>
     <div class="video-section">
         <HelpVideosItem
-            v-for="({ title }) in videos"
-            :title="title" />
+            v-for="({ title, video }, index) in videos"
+            :title="title"
+            :url="video"
+            :play="currentPlaying === index"
+            @pause="handlePause"
+            @play="handlePlay(index)" />
     </div>    
 </template>
 
