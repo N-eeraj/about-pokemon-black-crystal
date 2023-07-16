84045c81f608beb1d532e36131c0a37e9a7e6dc9
N-eeraj <neeraj7rajeena@gmail.com>
2023-07-16 09:11:18 +0530

    Added pokemon level progress indicator in FAQ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,13 @@
 import { resolve } from 'path'
 
 export default defineNuxtConfig({
-    app: {
-      baseURL: process.env.NODE_ENV === 'production' ? '/about-pokemon-black-crystal/' : '/'
-    },
-    alias: {
-      '@composables': resolve(__dirname, './composables'),
-      '@data': resolve(__dirname, './data'),
-      '@styles': resolve(__dirname, './styles'),
-      '@images': resolve(__dirname, './public/images')
-    }
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/about-pokemon-black-crystal/' : '/'
+  },
+  alias: {
+    '@composables': resolve(__dirname, './composables'),
+    '@data': resolve(__dirname, './data'),
+    '@styles': resolve(__dirname, './styles'),
+    '@images': resolve(__dirname, './public/images')
+  }
 })
